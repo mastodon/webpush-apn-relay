25ff0257469a064b6d0ca025d38258782fdce05f
Renaud Chaput <renchap@gmail.com>
2026-07-24 15:28:52 +0200

    Do not close body as it's nil and we dont read it

--- process_apn_notification.go
+++ process_apn_notification.go
@@ -95,8 +95,6 @@ func unsubscribe(unsubscribeUrl string, httpClient *http.Client, ctx context.Con
 		return false
 	}
 
-	defer unsubscribeReq.Body.Close()
-
 	unsubscribeResp, respErr := httpClient.Do(unsubscribeReq)
 	if respErr != nil {
 		requestLog.WithFields(log.Fields{
